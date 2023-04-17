1e53589dfa0ec5a18ca61b33d1b96f9299a2b2d9
Ihor Salfetnyk <4el@ukr.net>
2023-04-18 00:00:25 +0300

    add solution extended-repeater

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function renameFiles(names) {
+  const nameCounts = {};
+  const result = [];
+
+  for (const name of names) {
+    let newName = name;
+    if (nameCounts[newName]) {
+      const count = nameCounts[newName];
+      newName = `${newName}(${count})`;
+      nameCounts[newName] = 1;
+      nameCounts[name]++;
+    } else {
+      nameCounts[newName] = 1;
+    }
+    result.push(newName);
+  }
+
+  return result;
+} 
+
 
 module.exports = {
   renameFiles
